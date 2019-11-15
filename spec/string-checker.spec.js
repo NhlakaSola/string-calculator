1a9bcb820dd0048ce0d0b87c04ffb4e3089af94f
Mzito <you@example.com>
2019-11-15 14:36:29 +0200

    supports multiple delimiters

--- spec/string-checker.spec.js
+++ spec/string-checker.spec.js
@@ -1,5 +1,5 @@
 
-let calc = require('../src/string-checker');
+let calc = require('../src/string-calculator');
 
 let calculator = new calc;
 
@@ -12,4 +12,16 @@ describe("string calculator to add numbers", function () {
         expect(calculator.Add("1,2,3,4")).toEqual(10);
     });
 
+    it("should handle multiple delimiters of length longer than one char", function () {
+        expect(calculator.Add("//[**][%%]\n1**2%%3")).toEqual(6);
+    });   
+
+    it("should ignore numbers bigger than 1000", function () {
+        expect(calculator.Add("//;\n1002;23;2")).toEqual(25);
+    });
+
+
+
 });
+
+console.log(calculator.Add("//[;;;][***]\n-2;;-1000***3"));
--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,17 +1,22 @@
 module.exports = class StringCalculator{
 
       Add(string){     
-        let res = string.split(/[\n\,]/); //split string into res using \n & ,
+        let res = string.match(/\d{1,}/g); //only match digits only
         let summed = 0 ; 
         if (string == "") {
             return 0;
         }else{
             for (let i = 0; i < res.length; i++) {
                 let converted = parseInt(res[i]);  // convert splitted string into integer
+                if (converted >= 1000) {
+                    converted = 0;
+                }
                 summed += converted;  //summing the convert string 
             }return summed;
         }
 
     }
 
-}
+   }
+
+
